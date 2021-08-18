3ddcbfc4eebfab110d5eda907b92b1cf466252c6
user <KolchinVolo@gmail.com>
2021-08-18 18:53:12 +0300

    fixed v4 a bug with a pull request

--- assets/js/theme/custom.js
+++ assets/js/theme/custom.js
@@ -5,22 +5,17 @@ import 'slick-carousel';
 
 
 export default class Custom extends PageManager {
-
     constructor(context) {
         super(context);
         this.$body = $('body');
     }
-
     onReady() {
-
         setTimeout(function() {
             $('#login-form').modal({
                 fadeDuration: 250,
                 fadeDelay: 0.80
             });
         }, 4000);
-
-
     }
 
 }
